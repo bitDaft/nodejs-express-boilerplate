6964952597439d866308dafe01d3f71305abd4e5
Tausif ali <ajaxhis@tutanota.com>
2022-01-21 05:35:31 +0530

    preload all json files

--- bin/www.js
+++ bin/www.js
@@ -1,17 +1,18 @@
 #!/usr/bin/env node
-"use strict";
+'use strict';
 
 // # Application entry point.
-import http from "http";
+import http from 'http';
 
-import config from "#config";
-import app from "#app";
+import config from '#config';
+import app from '#app';
+import json from '#json';
 
 const port = config.PORT;
-app.set("port", port);
+app.set('port', port);
 
 const server = http.createServer(app);
 
 server.listen(port, () => {
-  console.log("Running -> Listening on port : " + port);
+  console.log('Running -> Listening on port : ' + port);
 });
