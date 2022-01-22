93d6e6477ce03c0db561de0c8f8e1ba280ee5ab5
Tausif ali <ajaxhis@tutanota.com>
2022-01-23 03:19:07 +0530

    cleanup

--- src/routes/test/index.js
+++ src/routes/test/index.js
@@ -1,10 +1,8 @@
 import express from 'express';
-import fs from 'fs';
 
 import { Pong } from '#controllers/test';
 import { Success } from '#lib/responseHelpers';
 import { __dirname } from '#lib/getFileDir';
-import path from 'path';
 
 const router = express.Router();
 
