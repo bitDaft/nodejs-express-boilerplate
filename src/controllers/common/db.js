df7523f6142f41907ed4909f0f7e60468fcfa544
Tausif ali <ajaxhis@tutanota.com>
2022-03-01 20:21:40 +0530

    removed common db

--- src/controllers/common/db.js
+++
@@ -1,4 +0,0 @@
-// # Add any common db queries used by the accompanying index file here
-// ! The queries have to be of the most simplest kind
-// ! Otherwise put them in their respective module controller folder
-// ! Even duplicate them in their modules db file, if they are not simple enough
