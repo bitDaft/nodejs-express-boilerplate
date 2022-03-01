2869fee80923a3197d169bd4ac03bf76b3565b4c
Tausif ali <ajaxhis@tutanota.com>
2022-03-01 20:18:26 +0530

    knex multi db fix metadata load

--- database/models/index.js
+++ database/models/index.js
@@ -6,9 +6,13 @@ import { initialize } from 'objection';
 import { knexI } from '#conns';
 
 // # Loads the metadata of all tables at app initialize in case it may be needed
-// ^ Add new models into this array too.
+// ^ Add new models into appropriate array too.
+const dbTables = {
+  0: [User, Role, RefreshToken],
+  1: [],
+};
 for (let key in knexI) {
-  await initialize(knexI[key], [User, Role, RefreshToken]);
+  await initialize(knexI[key], dbTables[key]);
 }
 
 export { User, Role, RefreshToken };
