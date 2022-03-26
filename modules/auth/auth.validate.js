f2bcc884b6df05d140f5aa3b75b32d1833fe27b0
Tausif ali <ajaxhis@tutanota.com>
2022-03-26 23:16:19 +0530

    fixed auth validate

--- modules/auth/auth.validate.js
+++ modules/auth/auth.validate.js
@@ -1,65 +1,65 @@
 import { Failure } from '#lib/responseHelpers';
 
 export const validateRegister = ({ name, email, password }, cb = (key, value) => {}) => {
-  if (typeof name !== 'string' && !name) {
+  if (typeof name !== 'string' || !name) {
     throw new Failure('Invalid name given');
   }
   cb('name', name);
-  if (typeof email !== 'string' && !email) {
+  if (typeof email !== 'string' || !email) {
     throw new Failure('Invalid email given');
   }
   cb('email', email);
-  if (typeof password !== 'string' && !password) {
+  if (typeof password !== 'string' || !password) {
     throw new Failure('Invalid password given');
   }
   cb('password', password);
 };
 
 export const validateLogin = ({ email, password }, cb = (key, value) => {}) => {
-  if (typeof email !== 'string' && !email) {
+  if (typeof email !== 'string' || !email) {
     throw new Failure('Invalid email given');
   }
   cb('email', email);
-  if (typeof password !== 'string' && !password) {
+  if (typeof password !== 'string' || !password) {
     throw new Failure('Invalid password given');
   }
   cb('password', password);
 };
 
 export const validateVerify = ({ token }, cb = (key, value) => {}) => {
-  if (typeof token !== 'string' && !token) {
+  if (typeof token !== 'string' || !token) {
     throw new Failure('Invalid verification token given');
   }
   cb('token', token);
 };
 
 export const validateRefreshToken = ({ refToken }, cb = (key, value) => {}) => {
-  if (typeof refToken !== 'string' && !refToken) {
+  if (typeof refToken !== 'string' || !refToken) {
     throw new Failure('Invalid refresh token given');
   }
   cb('refToken', refToken);
 };
 
 export const validateForgotPassword = ({ email }, cb = (key, value) => {}) => {
-  if (typeof email !== 'string' && !email) {
+  if (typeof email !== 'string' || !email) {
     throw new Failure('Invalid email given');
   }
   cb('email', email);
 };
 
 export const validateValidateResetToken = ({ token }, cb = (key, value) => {}) => {
-  if (typeof token !== 'string' && !token) {
+  if (typeof token !== 'string' || !token) {
     throw new Failure('Invalid reset token given');
   }
   cb('token', token);
 };
 
 export const validateRestPassword = ({ token, password }, cb = (key, value) => {}) => {
-  if (typeof token !== 'string' && !token) {
+  if (typeof token !== 'string' || !token) {
     throw new Failure('Invalid reset token given');
   }
   cb('token', token);
-  if (typeof password !== 'string' && !password) {
+  if (typeof password !== 'string' || !password) {
     throw new Failure('Invalid password given');
   }
   cb('password', password);
