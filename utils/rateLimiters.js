344220d7424c6426d46df1fc76f76f352fcf015c
Tausif ali <ajaxhis@tutanota.com>
2022-11-29 12:30:56 +0400

    added special rate limit for API keys

--- utils/rateLimiters.js
+++ utils/rateLimiters.js
@@ -2,6 +2,12 @@ import { RateLimiterMemory } from "rate-limiter-flexible";
 
 export const limiterMiddleware = new RateLimiterMemory({
   keyPrefix: "middleware",
+  points: 10,
+  duration: 1,
+});
+
+export const APIKeylimiterMiddleware = new RateLimiterMemory({
+  keyPrefix: "middleware",
   points: 100,
   duration: 1,
 });
