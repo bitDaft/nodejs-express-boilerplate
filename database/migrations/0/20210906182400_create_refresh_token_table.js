ddce08dc3b4816cba5cf3ed0afcb4d1ffb192fab
Tausif ali <ajaxhis@tutanota.com>
2022-12-01 00:56:48 +0400

    updated migrations

--- database/migrations/0/20210906182400_create_refresh_token_table.js
+++ database/migrations/0/20210906182400_create_refresh_token_table.js
@@ -3,9 +3,12 @@ export const up = (knex) => {
     t.increments('id').notNullable().unique().primary();
     t.integer('user_id').unsigned().notNullable();
     t.foreign('user_id').references('user.id').onDelete(knex.raw('CASCADE'));
+    t.integer('parent_id').unsigned();
+    t.foreign('parent_id').references('refresh_token.id').onDelete(knex.raw('CASCADE'));
     t.string('refresh_token').notNullable().unique();
     t.string('ua').notNullable().defaultTo('');
     t.datetime('expires').notNullable();
+    t.boolean('valid').notNullable().defaultTo(true);
     t.json('additional_information');
     t.datetime('created_at').defaultTo(knex.raw('CURRENT_TIMESTAMP')).notNullable();
     t.datetime('updated_at')
