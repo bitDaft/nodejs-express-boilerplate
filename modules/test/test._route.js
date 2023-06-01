361e02437d46a38684e3328060d613eb6985afed
Tausif ali <ajaxhis@tutanota.com>
2023-06-01 09:50:03 +0530

    updated test fns, updated packages

--- modules/test/test._route.js
+++ modules/test/test._route.js
@@ -1,11 +1,15 @@
 import express from 'express';
 
-import { ping } from '#controller/test';
+import { ping, slow, fast } from '#controller/test';
 
 const router = express.Router();
 
 const pingHandler = async (req, res) => await ping();
+const slowHandler = async (req, res) => await slow();
+const fastHandler = async (req, res) => await fast();
 
 router.get(`/ping`, pingHandler);
+router.get(`/slow`, slowHandler);
+router.get(`/fast`, fastHandler);
 
 export default router;
